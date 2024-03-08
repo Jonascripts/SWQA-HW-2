c6e8ba9f1305b1c667f0fea2b1df50ed95c6bca2
Jonas Cooper <123522856+Jonascripts@users.noreply.github.com>
2024-03-07 20:23:40 -0600

    Add Test cases for CheckHeightIn
    Test cases for CheckHeightIn added.

--- .vscode/BMI_Calculator_test.py
+++ .vscode/BMI_Calculator_test.py
@@ -47,5 +47,51 @@ class Test_TestCheckHeightFt(unittest.TestCase):
         self.assertEqual(BMI_Calculator.CheckHeightFt("-eight"), 0)
 
 
+class Test_TestCheckHeightIn(unittest.TestCase):
+
+    # Perform three tests of positive integers of varying size; all no greater than 12 should be accepted (return 1)
+    def test_posInt1(self):
+        self.assertEqual(BMI_Calculator.CheckHeightIn("5"), 1)
+
+    def test_posInt2(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("12"), 1)
+
+    def test_posInt3(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("35"), 0)
+
+
+    # Perform three tests of negative integers of different size; both should be rejected (return 0)
+    def test_negInt1(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("-5"), 0)
+
+    def test_negInt2(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("-12"), 0)
+
+    def test_negInt3(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("-35"), 0)
+
+
+    # Perform one test on integer zero; should be accepted (return 1)
+    def test_zeroInt(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("0"), 1)
+
+
+    # Perform five tests on non-integer values; all should be rejected (return 0)
+    def test_nonInt1(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("five"), 0)
+
+    def test_nonInt2(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("3and5"), 0)
+
+    def test_nonInt3(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt(";.#&"), 0)
+
+    def test_nonInt4(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("5."), 0)
+
+    def test_nonInt5(self):
+        self.assertEqual(BMI_Calculator.CheckHeightFt("-twelve"), 0)
+
+
 if __name__ == '__main__':
     unittest.main()
